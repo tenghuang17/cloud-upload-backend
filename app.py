b42fe38c0ea186e135997d5c853c9505682cb58a
tenghuang17 <128714099+tenghuang17@users.noreply.github.com>
2025-12-10 11:43:18 +0800

    try naming & file restrict(1st)

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 import hmac
+import uuid
 import os, boto3
 from flask import Flask, request, jsonify
 from flask_cors import CORS
+from datetime import datetime
 
 s3_client = boto3.client("s3")
 bucket_name = "upload-demo-nick"
@@ -17,6 +19,14 @@ CORS(app,
      ]}},
      allow_headers=["Content-Type", "Authorization"],
      methods=["POST", "OPTIONS"])
+def unified_filename_to_s3key():
+    #  日期資料夾，例如：20250120
+    date_prefix = datetime.utcnow().strftime("%Y%m%d")
+    # UUID  (32 字  hex，不含 dash)
+    unique_id = uuid.uuid4().hex  # "85df1c8e91bd4dd0ab57921c"
+    #  組出 key
+    s3_key = f"encrypted/{date_prefix}/{unique_id}_{filename}"
+    return s3_key
 
 @app.route("/get_URL", methods=["POST"])
 def get_URL():
@@ -26,16 +36,18 @@ def get_URL():
     # 遇無效JSON不丟錯，回傳None   data==送過來的整個body(JSON)格式
     if data is None:
         return jsonify({"error": "Invalid or missing JSON body"}), 400
-    filename = data.get("filename")
+    filename = data.get("filename") 
+    s3_key = unified_filename_to_s3key(filename)
     content_type = data.get("contentType", "application/octet-stream")
 
     presigned_url = s3_client.generate_presigned_url(
         "put_object",
-        Params={"Bucket": bucket_name, "Key": filename, "ContentType": content_type},
+        Params={"Bucket": bucket_name, "Key": s3_key, "ContentType": content_type},
         ExpiresIn=120  # 有效時間 (秒)
     )
 
-    return jsonify({"presigned_url": presigned_url, "key": filename}), 200
+    return jsonify({"presigned_url": presigned_url, "key": s3_key}), 200
+
 @app.route("/direct_uploadS3", methods=["POST"])
 def direct_uploadS3():
     if "file" not in request.files:
@@ -50,10 +62,11 @@ def direct_uploadS3():
 	"message":"upload to s3 success",
 	"key":key
     })
+
 @app.route("/upload_success",methods=["POST"])
-def upload_success():
+def upload_success():   #收前端通知 : 上傳s3成功
     data = request.get_json()
-    filename = data["filename"]
+    s3_key = data["key"]
     bucket = data["bucket"]
 
     print("got upload success noitce")
